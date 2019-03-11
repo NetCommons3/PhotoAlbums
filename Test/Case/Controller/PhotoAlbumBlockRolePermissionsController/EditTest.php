faa77bd593caf04696e025e0ca3d0af0875d004d
s-nakajima <nakajimashouhei@gmail.com>
2019-03-11 10:56:25 +0900

    phpunitエラー修正

--- Test/Case/Controller/PhotoAlbumBlockRolePermissionsController/EditTest.php
+++ Test/Case/Controller/PhotoAlbumBlockRolePermissionsController/EditTest.php
@@ -65,9 +65,9 @@ class PhotoAlbumBlockRolePermissionsControllerEditTest extends BlockRolePermissi
 			'PhotoAlbum' => array(
 				'id' => 2,
 				'blog_key' => 'blog_key_2',
-				'use_workflow' => true,
-				'use_comment_approval' => true,
-				'approval_type' => true,
+				'use_workflow' => '1',
+				'use_comment_approval' => '1',
+				'approval_type' => '1',
 			)
 		);
 
